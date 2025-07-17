486ca9343fcdf33ab8ebdb49e80ff12815d19505
Guillaume Riou <117857232+guillaumeriousat@users.noreply.github.com>
2025-07-17 09:55:54 -0400

    Adjust some free camera mouse behaviours  (#20)
    * invert left and right click and dont capture cursor in free cam
    
    * remove mouse velocity when not clicking
    
    * sensitivity changes

--- scripts/FreeCamera.gd
+++ scripts/FreeCamera.gd
@@ -6,7 +6,8 @@ const SHIFT_MULTIPLIER = 2.5
 const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
 
 
-@export_range(0.0, 1.0) var sensitivity: float = 0.05
+var right_click_sensitivity: float = 0.4
+var left_click_sensitivity: float = 0.15
 
 # Mouse state
 var _mouse_impulse = Vector2(0.0, 0.0)
@@ -46,21 +47,15 @@ func _unhandled_input(event):
 		return
 	# Receives mouse motion
 	if event is InputEventMouseMotion:
-		if _left_click_pressed and abs(event.relative[0])+abs(event.relative[0]) >=7.0:
-
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		_mouse_impulse += event.relative
 
 	# Receives mouse button input
 	if event is InputEventMouseButton:
 		match event.button_index:
 			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
 				_right_click_pressed = event.pressed
 			MOUSE_BUTTON_LEFT:
 				_left_click_pressed = event.pressed
-				if not event.pressed:
-					Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			MOUSE_BUTTON_WHEEL_DOWN:
 				_wheel_impulse +=5
 			MOUSE_BUTTON_WHEEL_UP:
@@ -94,6 +89,8 @@ func _unhandled_input(event):
 
 # Updates mouselook and movement every frame
 func _process(delta):
+	if not _left_click_pressed and not _right_click_pressed:
+		_mouse_impulse = Vector2.ZERO
 	_update_mouselook()
 	_update_movement(delta)
 
@@ -115,8 +112,8 @@ func _update_movement(delta):
 	if abs(_wheel_impulse) < 0.1:
 		_wheel_impulse = 0
 
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and _left_click_pressed:
-		_mouse_impulse *= sensitivity
+	if _right_click_pressed:
+		_mouse_impulse *= right_click_sensitivity
 		offset.x += _mouse_impulse[0]
 		offset.y -= _mouse_impulse[1]
 	_mouse_impulse *= 0.8
@@ -143,11 +140,10 @@ func _update_movement(delta):
 # Updates mouse look
 func _update_mouselook():
 	# Only rotates mouse if the mouse is captured
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and _right_click_pressed:
-		_mouse_impulse *= sensitivity
+	if _left_click_pressed:
+		_mouse_impulse *= left_click_sensitivity
 		var yaw = _mouse_impulse.x
 		var pitch = _mouse_impulse.y
-		_mouse_impulse = Vector2(0, 0)
 
 		# Prevents looking up/down too far
 		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
