2e230a6a4e6ccd9658e20bd818fdcce4602c8bd4
Guillaume Riou <griou@sat.qc.ca>
2025-08-26 15:55:10 -0400

    maaaaaaaybe ??

--- .github/macosx-notarize.sh
+++ .github/macosx-notarize.sh
@@ -27,37 +27,14 @@ codesign -vvv --force --deep --strict --sign "$MACOSX_SIGNATURE_IDENTITY" --opti
 
 echo "Uploading for notarization..."
 zip -r "$NAME.zip" "$NAME"
-xcrun altool --notarize-app -t osx -f "$NAME.zip" --primary-bundle-id "$MACOSX_BUNDLE_ID" -u "$MACOSX_APPLE_ID" -p "$MACOSX_APPLE_PASSWORD" --output-format xml > /tmp/notarize-app.xml
-rm -f "$NAME.zip"
-NUUID=`/usr/libexec/PlistBuddy -c 'Print :notarization-upload:RequestUUID' /tmp/notarize-app.xml`
-if [ -z "${NUUID}" ]; then
-    cat /tmp/notarize-app.xml
-    die "* error: no RequestUUID found in upload response"
-fi
-echo "RequestUUID: ${NUUID}"
-
-echo "Waiting for notarization to complete..."
-while true; do
-    xcrun altool --notarization-info ${NUUID} -u "$MACOSX_APPLE_ID" -p "$MACOSX_APPLE_PASSWORD" --output-format xml > /tmp/notarize-info.xml
-    NSTAT=`/usr/libexec/PlistBuddy -c 'Print :notarization-info:Status' /tmp/notarize-info.xml`
-    echo "  `date "+%H:%M:%S"` ${NSTAT}"
-    if [ -z "${NUUID}" ]; then
-        cat /tmp/notarize-info.xml
-        die "* error: no Status found in info response"
-    fi
-
-    if [ "${NSTAT}" == "invalid" ]; then
-        cat /tmp/notarize-info.xml
-        die "* error: error notarizing app"
-    fi
-
-    if [ "${NSTAT}" == "success" ]; then
-        break
-    fi
-    sleep 30s
-done
+xcrun notarytool submit "$NAME.zip" \
+              --team-id "62PMMWH49Z" \
+              --apple-id "glanelepine@gmail.com" \
+              --password "$MACOSX_APPLE_PASSWORD" \
+              --progress \
+              --wait
 
 echo "Stapling ticket to app..."
 xcrun stapler staple "$NAME"
 
-rm /tmp/notarize-*.xml
+#rm /tmp/notarize-*.xml
